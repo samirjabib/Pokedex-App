3d7585d561e5f09d29ffa40df1eaea3114f0e2dc
samir <samir.jc123@gmail.com>
2022-06-10 17:38:44 -0500

    git new changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import Pokedex from "./components/Pokedex";
+import ProtectedRoutes from "./components/ProtectedRoutes";
+import Settings from "./components/Settings";
+import UserInput from "./components/UserInput";
+import Pokemon from "./components/Pokemon"
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import "./styles/App.css"
+import { useState } from "react";
+import  styled, {ThemeProvider} from "styled-components";
+import {ligthTheme,darkTheme, GlobalStyles} from './components/hooks/themes'
+
+
+const StyledApp = styled.div`
+
+      color:${(props)=>props.theme.fontColor} 
+
+`
+
 
 function App() {
+
+
+  const [theme, setTheme] = useState("ligth")
+
+
+  const themeToggler = () =>{
+    if(theme === "ligth"){
+      setTheme("dark")
+    }else{
+      setTheme("ligth")
+    }
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={ theme === "ligth" ?  ligthTheme : darkTheme}>
+
+      <StyledApp className="App">
+        <GlobalStyles/>
+        <HashRouter>
+
+          <div className="pokeball-background"></div>
+
+
+          <div className="dark-mode-container">
+            <input 
+              type="checkbox" 
+              defaultChecked={theme}
+              onChange={() => themeToggler()}
+              className="dark-mode"
+              />
+          </div>
+          
+
+          <Routes>
+            {/* usamos el atributo Route para enlazar las pestañas */}
+            <Route path="/" element ={<UserInput/>}/>
+            {/* Rutas Protegidas usando el Atributo Route con apertura y cierre
+                usamos en element el enlace hacia el cual haremos la funcion */}
+            <Route element={<ProtectedRoutes/>}>
+              <Route path="/pokedex" element={<Pokedex/>}/>
+              <Route path="/pokedex/:id" element={<Pokemon/>}/>
+              <Route path="/settings" element={<Settings/>}/>
+            </Route>
+
+          </Routes>
+
+        </HashRouter>
+
+      </StyledApp>
+
+    </ThemeProvider>
   );
 }
 
